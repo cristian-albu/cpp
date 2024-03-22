dcf1c7a34b70b4b652d66dac4e7056a9e094052a
cristian-albu <albucristian95p@gmail.com>
2024-03-22 17:36:52 +0200

    binary search

--- pbinfo-exercises/003-arrays/binary-search/game-player/main.cpp
+++ pbinfo-exercises/003-arrays/binary-search/game-player/main.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 using namespace std;
 
-long long i, j;
+long long binary_search_closest_index(long long arr[], long long first, long long last, long long val) {
+    long long mid = first + (last - first) / 2;
 
-long long search(long long arr[], long long start, long long end,
-    long long input) {
+    if (last >= first) {
+        if (val == arr[mid])
+            return mid + 1;
 
-    if (end >= start) {
-        long long midd{ start + (end - start) / 2 };
+        if (arr[mid] > val)
+            return binary_search_closest_index(arr, mid + 1, last, val);
 
-        if (arr[midd] == input)
-            return midd;
-
-        if (arr[midd] > input)
-            return search(arr, start, midd - 1, input);
-
-        return search(arr, midd + 1, end, input);
-
+        return binary_search_closest_index(arr, first, mid - 1, val);
     }
-    return -1;
+
+    return mid;
 }
 
+
+
 int main() {
-    long long p_len, r_len;
+    long long i, p_len, r_len;
 
     cin >> p_len;
     long long players[p_len];
-    for (i = p_len - 1; i >= 0; --i)
+    for (i = 0; i < p_len; ++i)
         cin >> players[i];
 
     cin >> r_len;
@@ -34,10 +32,11 @@ int main() {
     for (i = 0; i < r_len; ++i)
         cin >> rounds[i];
 
-    long long res = search(players, 0, p_len - 1, 120);
 
-    cout << res;
-    cout << endl;
+    for (i = 0; i < r_len; ++i) {
+        long long res = binary_search_closest_index(players, 0, p_len - 1, rounds[i]);
+        cout << res << endl;
+    }
 
     return 0;
 }
